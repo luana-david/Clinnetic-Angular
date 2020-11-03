4f32af6e8c6f2c6156d91b59e8f4c65739842beb
DESKTOP-KL10C48\Luana <andreea.luana.david@gmail.com>
2020-11-03 20:52:51 +0200

    add nav and footer to common folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './common/nav/nav.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
