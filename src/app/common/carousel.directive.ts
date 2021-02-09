ebb8f8d8aa634ff8eb2e55afcc4dc671cb22b5e7
Luana David <andreea.luana.david@gmail.com>
2021-02-09 15:57:40 +0200

    change animation from slide to fadein-fade-out

--- src/app/common/carousel.directive.ts
+++ src/app/common/carousel.directive.ts
@@ -1,3 +1,4 @@
+import { group } from "@angular/animations";
 import { HostListener } from "@angular/core";
 import { AfterViewInit, Directive, ElementRef, Input } from "@angular/core";
 
@@ -8,53 +9,87 @@ import { AfterViewInit, Directive, ElementRef, Input } from "@angular/core";
 
 export class CarouselDirective implements AfterViewInit {
 
-  current = 2
+  current = 0
   direction: 'right' | 'left' | 'normal'
   width: number
+  childrenList: any[]
 
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit( ){
-    console.log(this.el.nativeElement.children[1].clientWidth)
     this.width = this.el.nativeElement.children[1].clientWidth
+    this.childrenList = this.el.nativeElement.children[1].children[0].children
+    console.log(this.childrenList[0].style);
     this.el.nativeElement.firstChild.addEventListener('click', this.onClickLeft.bind(this))
     this.el.nativeElement.lastChild.addEventListener('click', this.onClickRight.bind(this))
   }
 
   onClickLeft() {
-    if(this.current === 2) {
-      this.el.nativeElement.children[1].children[0].style.transform = 'translateX(' + this.width +'px)'
-    } else {
-      this.el.nativeElement.children[1].children[0].style.transform = 'translateX(0px)'
-    }
-    if(this.current > 1) {
+    // if(this.current === 2) {
+    //   this.el.nativeElement.children[1].children[0].style.transform = 'translateX(' + this.width +'px)'
+    // } else {
+    //   this.el.nativeElement.children[1].children[0].style.transform = 'translateX(0px)'
+    // }
+    // if(this.current > 1) {
+    //   this.current--
+    // }
+    // if(this.current === 1) {
+    //   this.el.nativeElement.firstChild.style.opacity = '0'
+    // } else {
+    //   this.el.nativeElement.lastChild.style.opacity = '1'
+    // }
+    if(this.current>0) {
       this.current--
-    }
-    if(this.current === 1) {
-      this.el.nativeElement.firstChild.style.opacity = '0'
     } else {
-      this.el.nativeElement.lastChild.style.opacity = '1'
+      this.current = this.childrenList.length-1
     }
+    this.styleChange()
   }
 
   onClickRight() {
-    if(this.current === 2) {
-      this.el.nativeElement.children[1].children[0].style.transform = 'translateX(-'+this.width+'px)'
-    } else {
-      this.el.nativeElement.children[1].children[0].style.transform = 'translateX(0px)'
-    }
-    if(this.current < 3) {
+    // if(this.current === 2) {
+    //   this.el.nativeElement.children[1].children[0].style.transform = 'translateX(-'+this.width+'px)'
+    // } else {
+    //   this.el.nativeElement.children[1].children[0].style.transform = 'translateX(0px)'
+    // }
+    // if(this.current < 3) {
+    //   this.current++
+    // }
+    // if(this.current === 3) {
+    //   this.el.nativeElement.lastChild.style.opacity = '0'
+    // } else {
+    //   this.el.nativeElement.firstChild.style.opacity = '1'
+    // }
+    if(this.current<this.childrenList.length-1) {
       this.current++
-    }
-    if(this.current === 3) {
-      this.el.nativeElement.lastChild.style.opacity = '0'
     } else {
-      this.el.nativeElement.firstChild.style.opacity = '1'
+      this.current = 0
+    }
+    this.styleChange()
+    console.log(this.current)
+  }
+
+  styleChange() {
+    for(let i=0; i<this.childrenList.length; i++) {
+      if(this.current === i) {
+        this.childrenList[i].style.opacity = "1"
+      } else {
+        this.childrenList[i].style.opacity ="0"
+      }
     }
   }
 
   @HostListener('window:resize', ['$event']) onResize(event) {
-    this.width = this.el.nativeElement.children[1].clientWidth
+
+    if(event.target.innerWidth>922) {
+      for(let i=0; i<this.childrenList.length; i++) {
+       this.childrenList[i].style.opacity = '1'
+     }
+   } else {
+    this.childrenList[0].style.opacity = '1'
+    this.childrenList[1].style.opacity = '0'
+    this.childrenList[2].style.opacity = '0'
+   }
   }
 
 }
