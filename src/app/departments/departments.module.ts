d6ec4dd0a2f1962a9f564d4a305364fb2edc3f67
Luana David <andreea.luana.david@gmail.com>
2021-03-10 18:30:49 +0200

    create child route to display doctors

--- src/app/departments/departments.module.ts
+++ src/app/departments/departments.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DepartmentsComponent } from './departments.component';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import { DoctorsComponent } from './doctors/doctors.component'
 
 const routes: Routes = [
-  {path: '', component: DepartmentsComponent}
+  {path: '', component: DepartmentsComponent, children: [
+    {
+      path: 'doctor/:id', component: DoctorsComponent
+    }
+  ]}
 ]
 
 @NgModule({
   declarations: [
-    DepartmentsComponent
+    DepartmentsComponent,
+    DoctorsComponent
   ],
   imports: [
     RouterModule.forChild(routes),
